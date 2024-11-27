7f08c541f4eb4ad096d1e3b188b30704a98c1090
NPN72 <155903865+NPN72@users.noreply.github.com>
2024-11-27 09:49:55 +0200

    Add err h Controller Advice

---
+++ api/src/main/java/com/tinqin/academy/api/enumerations/MessageLevel.java
@@ -0,0 +1,6 @@
+package com.tinqin.academy.api.enumerations;
+
+public enum MessageLevel {
+    ERROR,
+    WARNING
+}
--- rest/src/main/java/com/tinqin/academy/rest/controllers/BookController.java
+++ rest/src/main/java/com/tinqin/academy/rest/controllers/BookController.java
@@ -14,6 +14,7 @@ import com.tinqin.academy.api.querydemo.QueryDemo;
 import com.tinqin.academy.api.querydemo.QueryDemoInput;
 
 import com.tinqin.academy.api.querydemo.QueryDemoResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -65,7 +66,7 @@ public class BookController {
     }
 
     @PostMapping(APIRoutes.API_BOOK)
-    public ResponseEntity<?> createBook(@RequestBody CreateBookInput input) {
+    public ResponseEntity<?> createBook(@Valid @RequestBody CreateBookInput input) {
         CreateBookOutput result = createBook.process(input);
 
         return new ResponseEntity<>(result, HttpStatus.CREATED);
