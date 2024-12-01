439e6be6679b6fdf0e37614b3fb92cb3cef304a2
NPN72 <155903865+NPN72@users.noreply.github.com>
2024-12-01 10:58:02 +0200

    Changes to conflict file

---
+++ rest/src/main/java/com/tinqin/academy/rest/ConflictFile.java
@@ -0,0 +1,8 @@
+package com.tinqin.academy.rest;
+
+public class ConflictFile {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Print World");
+    }
+}
